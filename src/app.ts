c6f5109c76cd5d8fef8c072f7d14361df0ed761a
Jiasheng Lu <1473875731@qq.com>
2022-06-26 23:55:27 -0600

    feat: update files

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv'
-import routes from './routes';
 import helmet from 'helmet'
-import connectDB from './utils/connect';
+import connectDB from './utils/connectDB';
 import logger from './utils/logger';
-
+import router from './routes';
 // import environment variable form .env file
 dotenv.config()
 
@@ -12,36 +11,37 @@ dotenv.config()
 const PORT: number = +process.env.PORT!
 
 const app = express();
-app.use(helmet())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-const middleware =
-  ({ name }: { name: string }) =>
-  (req: Request, res: Response, next: NextFunction) => {
-    // @ts-ignore
-    req.name = name;
-    next();
-  };
+app.use(router)
+// app.use(helmet())
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-
-
-app.use(middleware({ name: 'testname' }));
+// const middleware =
+//   ({ name }: { name: string }) =>
+//   (req: Request, res: Response, next: NextFunction) => {
+//     // @ts-ignore
+//     req.name = name;
+//     next();
+//   };
 
 
-routes(app)
 
-async function throwError() {
-  throw new Error('Boom!')
-}
-app.get('/error', async (req, res) => {
-  try {
-    await throwError()
-    res.sendStatus(200)
-  } catch (e) {
-    res.status(400).send('happened')
-  }
-})
+// app.use(middleware({ name: 'testname' }));
+
+
+
+// async function throwError() {
+//   throw new Error('Boom!')
+// }
+// app.get('/error', async (req, res) => {
+//   try {
+//     await throwError()
+//     res.sendStatus(200)
+//   } catch (e) {
+//     res.status(400).send('happened')
+//   }
+// })
 
 
 app.listen(PORT, async () => {
---
+++ src/routes/user.routes.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.post('/api/user', (req, res) => { res.sendStatus(200) })
+
+export default router
